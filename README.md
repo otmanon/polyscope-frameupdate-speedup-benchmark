d2f255d818f9aadc5765b0368e58e7fc77c93c3e
otmanon <otman.benchekroun@mail.mcgill.ca>
2022-03-20 21:13:47 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+#fast frame update benchmark
+This benchmark is used to test how fast the frame update for polyscope is. We evaluate the speed on a 100k vertex volume mesh with 345k tets.
+
+To run the benchmark, clone the repo with 
+
+```
+git clone --recurse-submodules https://github.com/otmanon/polyscope-frameupdate-speedup-benchmark.git
+```
--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <igl/readMSH.h>
+#include <polyscope/volume_mesh.h>
 void main()
 {
+	Eigen::MatrixXd V;
+	Eigen::MatrixXi T, F;
+	Eigen::VectorXi tritag, tettag;
+	igl::readMSH("../data/woola.msh", V, F, T, tritag, tettag);
+
+	polyscope::init();
+	polyscope::registerVolumeMesh("Woola", V, T);
+
 	std::cout << "Hello Polyscope!" << std::endl;
+	polyscope::show();
 }
